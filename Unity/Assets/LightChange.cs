2222c525896498e57dc35a5d706873fbf0c9a5c7
sey2 <casio2978@naver.com>
2022-07-25 12:45:48 +0900

    Init Unity Project

---
+++ Unity/Assets/LightChange.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System;
+public class LightChange : MonoBehaviour
+{
+
+    /* light value define */
+    float duration  = 3.0f;
+    float originalRange;
+    float lightRange = 0.0f;
+    Light lt;
+    bool flag = true;    // Light Range = true: 0.0f -> 10.0f, false: 10.0f -> 0.0f
+    /* light value define */
+
+   /* Arduino Communication value define */
+    float airData = 0.0f;
+     public enum PortNumber{
+        COM1,   COM2,   COM3,   COM4,
+        COM5,   COM6,   COM7,   COM8,
+        COM9,   COM10,  COM11,  COM12,
+        COM13,  COM14,  COM15,  COM16
+    }
+
+    private SerialPort serial;
+
+    [SerializeField]
+    private PortNumber portNumber = PortNumber.COM4;
+    [SerializeField]
+    private string baudRate = "9600";
+     /* Arduino Communication  value define */
+
+
+    void Start()
+    {
+        InitArduinoCommunication();
+
+        lt = GetComponent<Light>();
+        originalRange = lt.range;
+    }
+    void Update()
+    {
+    
+        //  StartCoroutine("Fade");     // 조명의 값이 잘 바뀌는지 확인하고 싶으면 여기 있는 주석을 풀어주세요 
+
+        InputArduinoData();
+        lt.range = airData;
+    }
+
+    void InitArduinoCommunication(){
+        serial = new SerialPort(portNumber.ToString(), int.Parse(baudRate), Parity.None, 8, StopBits.One);
+        serial.Open();
+        serial.ReadTimeout = 5000;
+    }
+
+    void onApplicationQuit(){
+        serial.Close();
+    }
+
+    void InputArduinoData(){
+        if(serial.IsOpen){
+            try
+            {   
+                airData = float.Parse(serial.ReadLine());
+                Debug.Log(airData);
+                //airData = BitConverter.ToSingle(serial.ReadByte(), 0);
+                //Debug.Log("Air Pressure Sensor: " + airData);
+    
+            }
+            catch (System.Exception)
+            {
+                Debug.Log("error");
+                throw;
+            }
+        }else if(!serial.IsOpen){
+            serial.Open();
+        }
+    }
+
+        /* Coroutine Code 
+       test Light Range*/
+    IEnumerator Fade(){
+        if(lightRange >= 10.0f) flag = false;
+        else if(lightRange <= 0.0f) flag = true;
+
+        lightRange += (flag == true) ? 1.0f : -1.0f;
+        Debug.Log("LightRange: "+ lightRange);
+        lt.range = lightRange;
+
+        yield return new WaitForSeconds(0.5f);
+    }
+
+
+}
