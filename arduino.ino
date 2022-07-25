02cfbeffbd1eff38fe4ac00d2e08b7c999461dc5
aidan-choo <107440523+aidan-choo@users.noreply.github.com>
2022-07-24 20:24:46 -0400

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -1,10 +1,32 @@
-void setup() {
-  Serial.begin(9600);
+#include "HX710B.h"
 
+const int DOUT = 3;   //sensor data pin
+const int SCLK  = 2;   //sensor clock pin
+
+HX710B pressure_sensor; 
+
+void setup() {
+  Serial.begin(57600);
+  pressure_sensor.begin(DOUT, SCLK);
 }
 
 void loop() {
-  Serial.print("a");
-  delay(1000);
 
+  if (pressure_sensor.is_ready()) {
+    Serial.print("Pascal: ");
+    Serial.println(pressure_sensor.pascal());
+    /*
+    Serial.print("ATM: ");
+    Serial.println(pressure_sensor.atm());
+    Serial.print("mmHg: ");
+    Serial.println(pressure_sensor.mmHg());
+    Serial.print("PSI: ");
+    Serial.println(pressure_sensor.psi());
+    */
+  } else {
+    Serial.println("Pressure sensor not found.");
+  }
+
+  delay(1000);
+  
 }
