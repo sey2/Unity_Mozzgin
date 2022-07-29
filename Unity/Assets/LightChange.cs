c8ee9f4ea0bb83b24a7d04d2a1c87e18dc3f2bed
sey2 <casio2978@naver.com>
2022-07-29 13:52:47 +0900

    Add Arduino IO Check Code

--- Unity/Assets/LightChange.cs
+++ Unity/Assets/LightChange.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO.Ports;
 using System;
+using TMPro;
+
 public class LightChange : MonoBehaviour
 {
 
@@ -11,6 +13,7 @@ public class LightChange : MonoBehaviour
     float lightRange = 0.0f;
     Light lt;
     bool flag = true;    // Light Range = true: 0.0f -> 10.0f, false: 10.0f -> 0.0f
+    
     /* light value define */
 
    /* Arduino Communication value define */
@@ -30,10 +33,13 @@ public class LightChange : MonoBehaviour
     private string baudRate = "9600";
      /* Arduino Communication  value define */
 
+    [SerializeField] private TextMeshProUGUI ioCheckText;
+
+    public bool ioCheckFlag = false;
 
     void Start()
     {
-        //InitArduinoCommunication();
+        InitArduinoCommunication();
 
         lt = GetComponent<Light>();
         originalRange = lt.range;
@@ -43,8 +49,8 @@ public class LightChange : MonoBehaviour
     
         //  StartCoroutine("Fade");     // 조명의 값이 잘 바뀌는지 확인하고 싶으면 여기 있는 주석을 풀어주세요 
 
-       // InputArduinoData();
-        //lt.range = airData;
+        InputArduinoData();
+        lt.range = airData;
     }
 
     void InitArduinoCommunication(){
@@ -60,15 +66,18 @@ public class LightChange : MonoBehaviour
     void InputArduinoData(){
         if(serial.IsOpen){
             try
-            {   
+            {   ioCheckFlag = true;
                 airData = float.Parse(serial.ReadLine());
                 Debug.Log("Air Pressure Sensor: " + airData);
+                ioCheckText.text = "Air Pressure Sensor " + airData ;
                 lt.range = airData;
     
             }
             catch (System.Exception)
             {
+                ioCheckFlag = false;
                 Debug.Log("error");
+                ioCheckText.text = "Unable to read data from Arduino";
                 throw;
             }
         }else if(!serial.IsOpen){
--- Unity/Assets/SliderScripts.cs
+++ Unity/Assets/SliderScripts.cs
@@ -8,20 +8,28 @@ public class SliderScripts : MonoBehaviour
 {
     [SerializeField] private Slider slider;
     [SerializeField] private TextMeshProUGUI sliderText;
-
     [SerializeField] private Light lt;
+
+    GameObject lanternObj;
     // Start is called before the first frame update
     void Start()
     {
+        lanternObj = GameObject.Find("Lentern Light");
+
         slider.onValueChanged.AddListener((v)=>{
             sliderText.text = v.ToString("0.00");
-            lt.range = float.Parse(v.ToString("0.00"));
+
+            if(lanternObj.GetComponent<LightChange>().ioCheckFlag == false)
+              lt.range = float.Parse(v.ToString("0.00"));
+            else if(lanternObj.GetComponent<LightChange>().ioCheckFlag == true)
+              sliderText.text  = "Cannot change to slider because reading value with Arduino";
+
         });
     }
 
     // Update is called once per frame
     void Update()
     {
-    
+
     }
 }
